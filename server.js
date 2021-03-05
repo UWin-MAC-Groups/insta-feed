0f2fc13367b2af89982a96dee23bf01f37ac353b
Aniuchi Adaobi Michelle <adamichelllle@gmail.com>
2021-03-05 09:45:13 +0100

    Change body-parser to express-parser

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ const PORT = process.env.PORT || 4000;
 const app = express();
 
 app.use(cors())
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(express.static(path.join(__dirname,  "client", "build")))
 
 app.use('/api/v1', apiV1Routes);
