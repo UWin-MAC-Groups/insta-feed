0315148368899bd0d4862452e533d39671e4f761
Aniuchi Adaobi Michelle <adamichelllle@gmail.com>
2021-03-05 01:06:30 +0100

    Create server with routes

---
+++ server.js
@@ -0,0 +1,24 @@
+const dotenv = require('dotenv');
+const express = require("express");
+const bodyParser = require("body-parser");
+const startMongoServer = require('./src/config/db');
+const apiV1Routes = require('./src/routes/v1')
+
+dotenv.config();
+startMongoServer();
+
+const PORT = process.env.PORT || 4000;
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use('/api/v1', apiV1Routes);
+
+app.get("/", (req, res) => {
+  res.json({ message: "API Working" });
+});
+
+
+app.listen(PORT, (req, res) => {
+  console.log(`Server Started at PORT ${PORT}`);
+});
