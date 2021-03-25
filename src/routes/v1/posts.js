36988d5911c7cbceba7d65e768f3be9b411f4351
Aniuchi Adaobi Michelle <adamichelllle@gmail.com>
2021-03-25 14:06:09 +0100

    Add new routes

--- src/routes/v1/posts.js
+++ src/routes/v1/posts.js
@@ -1,11 +1,13 @@
 var express = require('express');
-const { PostController } = require('../../controllers');
+const { PostController, FileController } = require('../../controllers');
 var postRouter = express.Router();
 
 postRouter.get('/', PostController.viewFeed);
 postRouter.post('/', PostController.post);
 postRouter.get('/:id', PostController.viewPost);
 postRouter.post('/:id/comments', PostController.comment);
+postRouter.get('/:id/files', FileController.getFile);
+postRouter.post('/:id/files', FileController.upload);
 postRouter.put('/:id', PostController.like);
 postRouter.delete('/:id', PostController.delete);
 
