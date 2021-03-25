76ebc01238e0017ba3d25cbd18e4e9d1c0b4f771
Aniuchi Adaobi Michelle <adamichelllle@gmail.com>
2021-03-25 14:05:59 +0100

    Add controller methods for new routes

--- src/controllers/FileController.js
+++ src/controllers/FileController.js
@@ -1,5 +1,31 @@
-class FileController {
+const FileService = require('../services/FileService');
 
+class FileController {
+    static async upload(req, res) {
+        try {
+            const serviceRes = await FileService.uploadFile(req, res);
+            if(!serviceRes.success) 
+            return res.status(422).json({ status: 'error', message: serviceRes.message, data: null });
+
+            return res.status(201).json({ status: 'success', message: "File successfully uploaded", data: serviceRes.data.fileDetails });
+
+        } catch (error) {
+            return res.status(500).json({ status: 'error', message: "Internal server error", data: error.message })
+        }
+    }
+
+    static async getFile(req, res) {
+        try {
+            const serviceRes = await FileService.getChunkByPostId(req.params.id);
+            if(!serviceRes.success) 
+            return res.status(401).json({ status: 'error', message: serviceRes.message, data: null });
+
+            return res.status(200).json({ status: 'success', message: "File successfully retrieved", data: serviceRes.data.chunk });
+
+        } catch (error) {
+            return res.status(500).json({ status: 'error', message: "Internal server error", data: error.message })
+        }
+    }
 }
 
 module.exports = FileController;
